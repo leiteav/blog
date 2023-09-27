6543a01d1e9d758348937cc3345cf68820175295
Ariane Leite <arileitte@gmail.com>
2023-09-27 12:37:40 -0300

    fix: ajuste de configuração PasswordEncoder e add frameOptions().disable()

--- src/main/java/com/blog/blog/controller/AuthenticationController.java
+++ src/main/java/com/blog/blog/controller/AuthenticationController.java
@@ -2,35 +2,30 @@ package com.blog.blog.controller;
 
 import com.blog.blog.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
 public class AuthenticationController {
-    private AuthenticationManager authenticationManager;
+
+    private AuthenticationManager manager;
 
     @Autowired
     public AuthenticationController(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
+        this.manager = authenticationManager;
     }
 
     @PostMapping
-    public ResponseEntity authenticate(UserDTO userDTO){
+    public ResponseEntity login(@RequestBody UserDTO userDTO){
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDTO.getUsername(), userDTO.getPassword());
-        authenticationManager.authenticate(token);
+        manager.authenticate(token);
         return ResponseEntity.ok().build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
 }
